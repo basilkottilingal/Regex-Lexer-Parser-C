a5c57958812c3f08fac9959bb40348d46c9a5e17
basilkottilingal <basilkottilingal@gmail.com>
2025-09-01 13:12:22 +0200

    Working on lexer

--- include/allocator.h
+++ include/allocator.h
@@ -4,7 +4,6 @@
   .. A general pool that allocates memory with size in (0, 4088 Bytes].
   .. Warning. There is no memory freeing (except at the end of pgm).
   .. So repeated allocations may exhaust RAM.
-  .. May add a page deallocator so you recover pages (also allocate in pages).
   */
 
   #include <stdlib.h>
@@ -17,19 +16,15 @@
   #endif
 
   /* APIs
-  .. (a) "deallocate" all mem blocks. Call @ the end of pgm
+  .. (a) "destroy" all mem blocks. Call @ the end of pgm
   .. (b) "allocate" memory. max 4088 Bytes. (Rounded to 8 bytes. Maybe unnecessary)
   .. (c) "allocate_str" : equivalent to strdup ()
-  .. (d) allocate_stack(): sets a checkpoint so every allocate() after that can be
-  ..     freed just by allocate_unstack().
-  ..     Warning!! reusing any of the allocate() pointers b/w push & pop
-  ..     may result in unexpected behaviour
-  .. (e) allocate_unstack() gives back all memory allocated after the last
-  ..     allocate_stack(). Warning!! missed pop() may waste memory.
+  .. (d) "deallocate" a memory to proper bin.
+  .. 
   */
-  void   deallocate ();
+  void   destroy ();
   void * allocate ( size_t size );
   char * allocate_str ( const char * s );
-  void   allocate_stack ();
-  void   allocate_unstack ();
+  void   deallocate ( void *, size_t );
+  void   reallocate ( void *, size_t, size_t );
 #endif

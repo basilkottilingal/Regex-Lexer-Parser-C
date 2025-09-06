5025bb4d609c44d0e307dd68a9e7d0e8225c7f37
basil <basilkotttilingal@gmail.com>
2025-09-06 16:52:10 +0200

    working on equivalence classes.

--- include/bits.h
+++ include/bits.h
@@ -14,4 +14,27 @@
   
   #endif
 
+  #define BITBYTES(_b)            ( ((_b + 63) & ~(int)63) >> 3 )
+  #define BITCLEAR(_B,_s)         memset (_B, 0, _s);
+  #define BITLOOKUP(_B,_b)                                           \
+    ( _B[(_b)>>6] &  ((uint64_t)  1 << ((_b) & (int) 63)))
+  #define BITINSERT(_B,_b)                                           \
+      _B[(_b)>>6] |= ((uint64_t)  1 << ((_b) & (int) 63))
+  #define BITMINUS(_u,_v,_w,_s)                                      \
+    for (int _i = 0; _i < (_s>>3); ++_i) {                           \
+      _w[_i] = _u[_i] & ~_v[_i];                                     \
+    }
+  #define BITAND(_u,_v,_w,_c,_s)  do { _c = 0;                       \
+    for (int _i = 0; _i < (_s>>3); ++_i) {                           \
+      _c |= (_w[_i] = _u[_i] & _v[_i]) != 0;                         \
+    } } while (0)
+  #define BITCMP(_u,_v,_c,_s)     do { _c = 1;                       \
+    for (int _i = 0; _c && _i < (_s>>3); ++_i) {                     \
+      _c = (_u[_i] == _v[_i]);                                       \
+    } } while (0)
+  #define BITCOUNT(_B,_c,_s)      do { _c = 0;                       \
+    for (int _i = 0; _i < (_s>>3); ++_i) {                           \
+      _c += __builtin_popcountll( _B[_i] );                          \
+    } } while (0)
+
 #endif

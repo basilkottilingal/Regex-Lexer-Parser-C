7ce65b6e66225ee41b4e7cae28cb88a9c9443658
basil <basilkotttilingal@gmail.com>
2025-09-23 13:18:03 +0200

    Lxr requires, testing and bdry assertions.

---
+++ test/input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../src/source.c"
+
+int main () {
+  FILE * fp = fopen ("../src/source.c", "r");
+
+  lxrin_set (fp);
+
+  int c;
+  while ( (c = lxr_input ()) != EOF) {
+    printf ("%c", (char) c);
+  };
+
+  //clean??
+
+  fclose (fp);
+
+  return 0;
+}
+
